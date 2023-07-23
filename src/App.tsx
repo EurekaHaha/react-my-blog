431f2401f4211ebd1df1fd509bf916777dd28be9
wangzhaow <wangzhaow@yonyou.com>
2023-07-23 16:10:13 +0800

    feat: add loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 // import { useState } from 'react'
+import { CircularProgress } from '@mui/material';
 import { useRoutes } from 'react-router-dom';
 import routes from './routes';
 import './App.css';
+import { totalSettingState } from './store/features/totalSettingSlice';
+import { useAppSelector } from './store/hooks';
 function App() {
   const routesElement = useRoutes(routes);
-  return routesElement;
+  const state = useAppSelector(totalSettingState);
+  const { loading } = state;
+  return (
+    <>
+      {loading && (
+        <div className="background_loading">
+          <CircularProgress />
+        </div>
+      )}
+      {routesElement}
+    </>
+  );
 }
 
 export default App;
